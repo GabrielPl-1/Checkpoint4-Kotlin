8fee0da73f23d6aa28f24775365406ae2a67852e
Gabriel Penna <gabrielpl@al.insper.edu.br>
2024-09-14 10:41:04 -0300

    Adicionando a tela principal

--- app/src/main/java/gabrielpl1/com/github/listadecompras/MainActivity.kt
+++ app/src/main/java/gabrielpl1/com/github/listadecompras/MainActivity.kt
@@ -14,33 +14,7 @@ import gabrielpl1.com.github.listadecompras.ui.theme.ListaDeComprasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
-        setContent {
-            ListaDeComprasTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ListaDeComprasTheme {
-        Greeting("Android")
     }
 }
